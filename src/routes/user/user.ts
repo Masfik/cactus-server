03ce25705ff3e8b9bab0954502990a38fb783296
Masfik <contact@masfik.net>
2020-06-29 23:55:06 +0100

    Created POST /user and installed body-parser dependency

--- src/routes/user/user.ts
+++ src/routes/user/user.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { firebaseUser } from "../../middlewares/firebase-user";
 import { UserModel as users } from "../../database/UserSchema";
+import { User } from "../../models/user";
 
 export const router = Router();
 
-router.get("/", firebaseUser, async (req, res) => {
+router.get("/", firebaseUser, (req, res) => {
   const { username, id } = req.query;
 
   if (username === null && id === null) {
@@ -22,6 +23,21 @@ router.get("/", firebaseUser, async (req, res) => {
       .then(res.json)
       .catch(() => res.status(404).json({ error: "user not found" }));
   } else res.status(400);
+});
+
+router.post("/", firebaseUser, async (req, res) => {
+  if (res.locals.user /* If the user already exists in the database */) {
+    res.status(400);
+    return;
+  }
+
+  const { name, surname, username, email } = req.body;
+
+  if (name && surname && username && email)
+    await users
+      .create(<User>{ name, surname, username, email })
+      .catch(() => res.status(400));
+  else res.status(400).json({ error: "invalid json data" });
 });
 
 router.get("/search", async (req, res) => {
